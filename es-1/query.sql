be9b7c99c844d256ecfe2b6c096c8e8692c209f1
Alex Carrega <contact@alexcarrega.com>
2024-04-16 15:29:36 +0200

    Technical Test

--- es-1/query.sql
+++ es-1/query.sql
@@ -1,5 +1,5 @@
 SELECT
     (SUM(CASE WHEN OrderType.name = 'Contrassegno' THEN Order.price * Order.quantity ELSE 0 END) / SUM(Order.price * Order.quantity)) * 100 AS PercentualeContrassegno,
     (SUM(CASE WHEN OrderType.name = 'Online' THEN Order.price * Order.quantity ELSE 0 END) / SUM(Order.price * Order.quantity)) * 100 AS PercentualeOnline
-FROM Order, OrderType
-WHERE Order.type_id = OrderType.id;
+FROM Order, OrderType, Article
+WHERE Order.type_id = OrderType.id AND Article.color = 'black' AND Article.id = Order.article_id AND Article.quantity > 0;
